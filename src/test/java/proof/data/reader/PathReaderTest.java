3f0411781d74243f34a3fca409fbf1b1f31aaae4
Tilo Wiedera <Tilo@Wiedera.de>
2014-09-21 15:13:47 +0200

    Refactored PathReaderTest

--- src/test/java/proof/data/reader/PathReaderTest.java
+++ src/test/java/proof/data/reader/PathReaderTest.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 
 import proof.ResourceBasedTest;
@@ -18,25 +19,34 @@ import proof.exception.InvalidPathException;
 
 public class PathReaderTest extends ResourceBasedTest {
 
+  private Graph graph;
+  private PathReader reader;
+  private HashSet<CrossingIndex> crossings;
+
   public PathReaderTest() {
     super("path-reader");
   }
 
+  @Before
+  public void init() {
+    crossings = new HashSet<CrossingIndex>();
+    graph = createCompleteGraph(10);
+    reader = new PathReader(graph, crossings);
+  }
+
   @Test(expected = InvalidPathException.class)
   public void testValidate_empty() throws InvalidPathException, JSONException {
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(new JSONArray());
+    reader.read(new JSONArray());
   }
 
   @Test(expected = InvalidPathException.class)
   public void testValidate_disconnected() throws InvalidPathException, JSONException {
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(loadJSON(
-        "disconnected").getJSONArray("path"));
+    reader.read(loadJSON("disconnected").getJSONArray("path"));
   }
 
   @Test(expected = InvalidPathException.class)
   public void testValidate_missingEdge() throws InvalidPathException, JSONException {
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(loadJSON(
-        "missing-edge").getJSONArray("path"));
+    reader.read(loadJSON("missing-edge").getJSONArray("path"));
   }
 
   @Test(expected = InvalidPathException.class)
@@ -44,46 +54,34 @@ public class PathReaderTest extends ResourceBasedTest {
     JSONArray path = loadJSON("multi-edges").getJSONArray("path");
     JSONObject section = path.getJSONObject(0);
     section.put("keepDirection", !section.getBoolean("keepDirection"));
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(path);
+
+    reader.read(path);
   }
 
   @Test
   public void testValidate_simple() throws InvalidPathException, JSONException {
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(loadJSON("simple")
-        .getJSONArray("path"));
+    reader.read(loadJSON("simple").getJSONArray("path"));
   }
 
   @Test
   public void testValidate_simpleWithVariables() throws InvalidPathException, JSONException {
-    Set<CrossingIndex> crossings = new HashSet<CrossingIndex>();
-
     crossings.add(new CrossingIndex(new SegmentIndex(2, 3), new SegmentIndex(4, 5)));
     crossings.add(new CrossingIndex(new SegmentIndex(2, 10), new SegmentIndex(3, 5)));
     crossings.add(new CrossingIndex(new SegmentIndex(7, 3), new SegmentIndex(5, 10)));
 
-    new PathReader(createCompleteGraph(10), crossings)
-        .read(loadJSON("simple").getJSONArray("path"));
+    reader.read(loadJSON("simple").getJSONArray("path"));
   }
 
   @Test
   public void testValidate_multi() throws InvalidPathException, JSONException {
-    new PathReader(createCompleteGraph(10), new HashSet<CrossingIndex>()).read(loadJSON(
-        "multi-edges").getJSONArray("path"));
+    reader.read(loadJSON("multi-edges").getJSONArray("path"));
   }
 
   @Test
   public void testValidate_crossing() throws InvalidPathException, JSONException {
-    Set<CrossingIndex> crossings = new HashSet<CrossingIndex>();
-
     JSONArray path = loadJSON("crossing").getJSONArray("path");
-    Graph graph = createCompleteGraph(10);
-    PathReader reader = new PathReader(graph, crossings);
 
-    try {
-      reader.read(path);
-      fail("Should detect missing variable");
-    } catch (InvalidPathException expected) {
-    }
+    assertInvalid(path);
 
     crossings.add(new CrossingIndex(new SegmentIndex(graph.getEdgeId(0, 3), 3), new SegmentIndex(
         graph.getEdgeId(1, 4), 5)));
@@ -92,9 +90,9 @@ public class PathReaderTest extends ResourceBasedTest {
   }
 
   @Test
-  public void testValidate_crossingsAsEndpoints() throws InvalidPathException, JSONException {
+  public void testValidate_crossingExtended() throws InvalidPathException, JSONException {
     Set<CrossingIndex> crossings = new HashSet<CrossingIndex>();
-    JSONArray path = loadJSON("crossings-as-endpoints").getJSONArray("path");
+    JSONArray path = loadJSON("crossing-2").getJSONArray("path");
 
     Graph graph = createCompleteGraph(10);
     PathReader reader = new PathReader(graph, crossings);
@@ -105,26 +103,25 @@ public class PathReaderTest extends ResourceBasedTest {
     edge = path.getJSONObject(path.length() - 1).getJSONObject("edge");
     int lastEdge = graph.getEdgeId(edge.getInt("source"), edge.getInt("target"));
 
-    try {
-      reader.read(path);
-      fail("Should detect missing variable");
-    } catch (InvalidPathException expected) {
-    }
+    assertInvalid(path);
 
     crossings.add(new CrossingIndex(new SegmentIndex(firstEdge, path.getJSONObject(0).getInt(
         "start")), new SegmentIndex(5, 5)));
 
-    try {
-      reader.read(path);
-      fail("Should detect missing variable");
-    } catch (InvalidPathException expected) {
-    }
-
+    assertInvalid(path);
     crossings.clear();
     crossings.add(new CrossingIndex(new SegmentIndex(firstEdge, path.getJSONObject(0).getInt(
         "start")),
         new SegmentIndex(lastEdge, path.getJSONObject(path.length() - 1).getInt("start"))));
 
     reader.read(path);
+  }
+
+  private void assertInvalid(JSONArray path) {
+    try {
+      reader.read(path);
+      fail("Should detect missing variable");
+    } catch (InvalidPathException expected) {
+    }
   }
 }
